3b749a62d0707c97c6863332e268797a5fd8e20b
xuziyong <www5555013@163.com>
2019-06-05 15:25:40 +0800

    [修改]【EasyPlayer】添加了文字测试

---
+++ src/components/AmapTest/AmapTest.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "AmapTest"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ElementFormRender/ElementFormRender.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ElementFormRender"
+    }
+</script>
+
+<style scoped>
+
+</style>
